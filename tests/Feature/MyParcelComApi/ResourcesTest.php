122b2f7eee5545394f6e0f0c35cec4f876ddc6fe
Nick de Vries <nick@myparcel.com>
2024-01-11 10:16:22 +0100

    :heavy_division_sign: divide MyParcelComApi tests in separate files

---
+++ tests/Feature/MyParcelComApi/ResourcesTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Feature\MyParcelComApi;
+
+use DateTime;
+use MyParcelCom\ApiSdk\Resources\Address;
+use MyParcelCom\ApiSdk\Resources\Interfaces\FileInterface;
+use MyParcelCom\ApiSdk\Resources\Interfaces\ResourceInterface;
+use MyParcelCom\ApiSdk\Resources\Interfaces\ShopInterface;
+use MyParcelCom\ApiSdk\Tests\TestCase;
+
+/**
+ * @group Carriers
+ */
+class ResourcesTest extends TestCase
+{
+    public function testGetResourceById()
+    {
+        /** @var FileInterface $file */
+        $file = $this->api->getResourceById(ResourceInterface::TYPE_FILE, 'file-id-1');
+        $this->assertInstanceOf(FileInterface::class, $file);
+        $this->assertEquals('files', $file->getType());
+        $this->assertEquals('file-id-1', $file->getId());
+        $this->assertEquals('label', $file->getDocumentType());
+        $this->assertEquals([['extension' => 'pdf', 'mime_type' => 'application/pdf']], $file->getFormats());
+
+        /** @var ShopInterface $shop */
+        $shop = $this->api->getResourceById(ResourceInterface::TYPE_SHOP, 'shop-id-1');
+        $this->assertInstanceOf(ShopInterface::class, $shop);
+        $this->assertEquals('shops', $shop->getType());
+        $this->assertEquals('shop-id-1', $shop->getId());
+        $this->assertEquals('Testshop', $shop->getName());
+        $this->assertEquals((new DateTime())->setTimestamp(1509378904), $shop->getCreatedAt());
+        $this->assertEquals(
+            (new Address())
+                ->setStreet1('Hoofdweg')
+                ->setStreetNumber(679)
+                ->setPostalCode('1AA BB2')
+                ->setCity('London')
+                ->setCountryCode('GB')
+                ->setFirstName('Mister')
+                ->setLastName('Sender')
+                ->setCompany('MyParcel.com')
+                ->setEmail('info@myparcel.com')
+                ->setPhoneNumber('+31 85 208 5997'),
+            $shop->getSenderAddress()
+        );
+        $this->assertEquals(
+            (new Address())
+                ->setStreet1('Hoofdweg')
+                ->setStreetNumber(679)
+                ->setPostalCode('1AA BB2')
+                ->setCity('London')
+                ->setCountryCode('GB')
+                ->setFirstName('Mister')
+                ->setLastName('Return')
+                ->setCompany('MyParcel.com')
+                ->setEmail('info@myparcel.com')
+                ->setPhoneNumber('+31 85 208 5997'),
+            $shop->getReturnAddress()
+        );
+    }
+}
---
+++ tests/Feature/MyParcelComApi/ShopsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Feature\MyParcelComApi;
+
+use MyParcelCom\ApiSdk\Collection\CollectionInterface;
+use MyParcelCom\ApiSdk\Resources\Interfaces\ShopInterface;
+use MyParcelCom\ApiSdk\Tests\TestCase;
+
+/**
+ * @group Shops
+ */
+class ShopsTest extends TestCase
+{
+    public function testGetShops()
+    {
+        $shops = $this->api->getShops();
+
+        $this->assertInstanceOf(CollectionInterface::class, $shops);
+        foreach ($shops as $shop) {
+            $this->assertInstanceOf(ShopInterface::class, $shop);
+        }
+    }
+
+    public function testGetDefaultShop()
+    {
+        $shop = $this->api->getDefaultShop();
+        $this->assertInstanceOf(ShopInterface::class, $shop);
+    }
+}
