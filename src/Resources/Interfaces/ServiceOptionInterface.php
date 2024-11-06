565a35ab652b6df696c9b48eb032a48cc1b079e5
Martin Boer <martin@myparcel.com>
2024-11-06 14:29:47 +0100

    :memo: briefly explain the correlation of the values_format and values on a ServiceOption

--- src/Resources/Interfaces/ServiceOptionInterface.php
+++ src/Resources/Interfaces/ServiceOptionInterface.php
@@ -20,6 +20,9 @@ interface ServiceOptionInterface extends ResourceInterface
 
     public function setValuesFormat(?array $valuesFormat): self;
 
+    /**
+     * Defines if this ServiceOption requires a `values` array to be set when using it to create a shipment.
+     */
     public function getValuesFormat(): ?array;
 
     public function setPrice(?int $price): self;
@@ -34,6 +37,10 @@ interface ServiceOptionInterface extends ResourceInterface
 
     public function isIncluded(): bool;
 
+    /**
+     * When adding a ServiceOption to a Shipment, it might require values such as an `amount` and a `currency`.
+     * This is defined in the `values_format` of the ServiceOption, which returns these requirements as JSON Schema.
+     */
     public function setValues(?array $values): self;
 
     public function getValues(): ?array;
